7ee2a45c6bee227d7451bb7fdbd2468c1a94bc27
Marcel Kaneko <marcel.kaneko@gmail.com>
2019-08-26 18:50:41 -0300

    sketchSimpleWebServerESP32
    Call on web server to get result. Updates result from serial ports (2)

---
+++ sketchSimpleWebServerESP32/sketchSimpleWebServerESP32.ino
@@ -0,0 +1,163 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <HardwareSerial.h>
+ 
+#define RXD2 16
+#define TXD2 17
+
+/*Put your SSID & Password*/
+const char* ssid = "Kanekos";  // Enter SSID here
+const char* password = "kanekomarcel";  //Enter Password here
+
+WebServer server(80);
+
+uint8_t LED1pin = 4;
+bool LED1status = LOW;
+
+uint8_t LED2pin = 5;
+bool LED2status = LOW;
+
+String valorRes = "";
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  
+  delay(100);
+  pinMode(LED1pin, OUTPUT);
+  pinMode(LED2pin, OUTPUT);
+
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  //connect to your local wi-fi network
+  WiFi.begin(ssid, password);
+
+  //check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+
+  //server.on("/", handle_OnConnect);
+  server.on("/", handle_Serial);
+  //server.on("/led1on", handle_led1on);
+  //server.on("/led1off", handle_led1off);
+  //server.on("/led2on", handle_led2on);
+  //server.on("/led2off", handle_led2off);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+void loop() {
+  server.handleClient();
+  //if(LED1status)
+  //{digitalWrite(LED1pin, HIGH);}
+  //else
+  //{digitalWrite(LED1pin, LOW);}
+  
+  //if(LED2status)
+  //{digitalWrite(LED2pin, HIGH);}
+  //else
+  //{digitalWrite(LED2pin, LOW);}
+  String conteudo = "";
+  char caractere;
+  if (Serial2.available() > 0) {
+    while (Serial2.available()) {
+          // Lê byte da serial
+      caractere = char(Serial2.read());
+      // Ignora caractere de quebra de linha
+      if (caractere != '\n' && caractere != '\r') {
+        // Concatena valores
+        conteudo.concat(caractere);
+      }
+      // Aguarda buffer serial ler próximo caractere
+      delay(10);
+    }
+    Serial.println(valorRes);
+    valorRes = conteudo;
+  }
+  
+  Serial2.print("5");
+  Serial2.println();
+  Serial.println("Enviado comando");
+  delay (2000);
+}
+
+void handle_Serial() {
+  Serial.println("Send Result: " + valorRes);
+  server.send(200, "text/html", valorRes);
+}
+
+void handle_OnConnect() {
+  LED1status = LOW;
+  LED2status = LOW;
+  Serial.println("GPIO4 Status: OFF | GPIO5 Status: OFF");
+  server.send(200, "text/html", SendHTML(LED1status,LED2status)); 
+}
+
+void handle_led1on() {
+  LED1status = HIGH;
+  Serial.println("GPIO4 Status: ON");
+  server.send(200, "text/html", SendHTML(true,LED2status)); 
+}
+
+void handle_led1off() {
+  LED1status = LOW;
+  Serial.println("GPIO4 Status: OFF");
+  server.send(200, "text/html", SendHTML(false,LED2status)); 
+}
+
+void handle_led2on() {
+  LED2status = HIGH;
+  Serial.println("GPIO5 Status: ON");
+  server.send(200, "text/html", SendHTML(LED1status,true)); 
+}
+
+void handle_led2off() {
+  LED2status = LOW;
+  Serial.println("GPIO5 Status: OFF");
+  server.send(200, "text/html", SendHTML(LED1status,false)); 
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(uint8_t led1stat,uint8_t led2stat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>LED Control</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr +=".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+  ptr +=".button-on {background-color: #3498db;}\n";
+  ptr +=".button-on:active {background-color: #2980b9;}\n";
+  ptr +=".button-off {background-color: #34495e;}\n";
+  ptr +=".button-off:active {background-color: #2c3e50;}\n";
+  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<h1>ESP32 Web Server</h1>\n";
+    ptr +="<h3>Using Station(STA) Mode</h3>\n";
+  
+   if(led1stat)
+  {ptr +="<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";}
+  else
+  {ptr +="<p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";}
+
+  if(led2stat)
+  {ptr +="<p>LED2 Status: ON</p><a class=\"button button-off\" href=\"/led2off\">OFF</a>\n";}
+  else
+  {ptr +="<p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a>\n";}
+
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
